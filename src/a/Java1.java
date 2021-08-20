13f5df8427ba172c26d32ac456993217c34feae9
bharat <balu.bharat39@gmail.com>
2021-08-20 15:10:45 +0530

    I am a new employee done new changes

--- src/a/Java1.java
+++ src/a/Java1.java
@@ -7,11 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Java1{   
 	public static void main( String[] args) throws InterruptedException
 	{
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
-		driver.findElement(By.xpath("//div[.='Sign in']")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.id("identifierId")).sendKeys("balu.bharat39@gmail.com");
+		System.out.println("Welcome");
 
 	}
 	 
